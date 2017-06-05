ad3ee4791a151532684139f0e0fb0276f71dbe26
Aart den Hoed <aart_den_hoed@hotmail.com>
2017-06-05 12:38:48 +0200

    Datacontract added + models voor inlezen WMIC file

---
+++ ConfigMan/ConfigMan/Models/Installation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ConfigMan.Models
+{
+    public partial class Installation
+    {
+        [NotMapped]
+        public string Vendor;
+        [NotMapped]
+        public String Component;
+    }
+}
---
+++ ConfigMan/ConfigMan/Models/WMIrapport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ConfigMan.Models
+{
+    [DataContract]
+    public class WMIrapport
+    {
+        [DataMember]
+        public string ReportedByNode;
+        [DataMember]
+        public List<Installation> HighestReleaseList;
+        [DataMember]
+        public List<ReportedComputer> ComputerList;
+    }
+}
