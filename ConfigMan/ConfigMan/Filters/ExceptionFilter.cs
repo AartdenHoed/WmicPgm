88f6dc765fbf38aae7e00281ea149a61033061ba
Aart den Hoed <aart_den_hoed@hotmail.com>
2017-06-18 15:27:24 +0200

    Zie voorgaande commit

---
+++ ConfigMan/ConfigMan/Filters/ExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConfigMan.Filters
+{
+    public class ExceptionFilter : HandleErrorAttribute
+    {
+    
+        public override void OnException(ExceptionContext filterContext)
+        {
+            string logmsg = "Message: " + filterContext.Exception.Message ;
+            Debug.WriteLine(logmsg, "Error Filter Log");
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
+       
---
+++ ConfigMan/ConfigMan/Filters/LogActionFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+namespace ConfigMan.ActionFilters
+
+{
+    public class LogActionFilter : ActionFilterAttribute
+
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log("OnActionExecuting", filterContext.RouteData);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("OnActionExecuted", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log("OnResultExecuting", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("OnResultExecuted", filterContext.RouteData);
+        }
+
+
+        private void Log(string methodName, RouteData routeData)
+        {
+            string logmsg = "";
+            logmsg = methodName;
+            foreach (KeyValuePair<string, object> kvp in routeData.Values ) {
+                logmsg = logmsg + " - " + kvp.Key + ": " + kvp.Value;
+            }
+            Debug.WriteLine(logmsg, "Action Filter Log");
+        }
+
+    }
+}
